7b1ecf98acfa1375c19d3791fbba64f7109e0010
YangAidi <598671587@qq.com>
2018-04-02 23:12:18 +0800

    problem186

--- src/Solution186.java
+++ src/Solution186.java
@@ -1,12 +1,11 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @program: LintCode
  * @author: YangAidi
  * @create: 2018-04-02 09:50
+ * 自己的暴力解法还是不行啊，始终过不了测试用例，不知道什么原因。不过也发现刚开始的暴力解法有问题。
+ * 就是自己的技术习惯性是j=i+1，想着能节省一点时间是一点，但是这样，i不能出现在i+1,i+2的直线上。
  **/
 
 // Definition for a point.
@@ -149,54 +148,146 @@ class Point {
 //        return max;
 //    }
 //}
-public class Solution186 {
-    /**
-     * @param points an array of point
-     * @return an integer
-     */
-    public int maxPoints(Point[] points) {
-        // Write your code here
-        if (points == null) {
-            return 0;
-        }
-        int ans = 0;
-        for (int i = 0; i < points.length; i++) {
-            Map<String, Integer> slope = new HashMap<>();
-            int maxPoints = 0, overlap = 0, vertical = 0;
+//public class Solution186 {
+//    /**
+//     * @param points an array of point
+//     * @return an integer
+//     */
+//    public int maxPoints(Point[] points) {
+//        // Write your code here
+//        if (points == null) {
+//            return 0;
+//        }
+//        int ans = 0;
+//        for (int i = 0; i < points.length; i++) {
+//            Map<String, Integer> slope = new HashMap<>();
+//            int maxPoints = 0, overlap = 0, vertical = 0;
+//
+//            for (int j = i + 1; j < points.length; j++) {
+//                if (points[i].x == points[j].x) {
+//                    if (points[i].y == points[j].y) {
+//                        overlap++;
+//                    } else {
+//                        vertical++;
+//                    }
+//                    continue;
+//                }
+//                int dx = points[i].x - points[j].x;
+//                int dy = points[i].y - points[j].y;
+//                int tmp = gcd(dx, dy);
+//                dx /= tmp;
+//                dy /= tmp;
+//                String k = dy + "/" + dx;
+//
+//                if (!slope.containsKey(k)) {
+//                    slope.put(k, 0);
+//                }
+//                slope.put(k, slope.get(k) + 1);
+//                maxPoints = Math.max(maxPoints, slope.get(k));
+//            }
+//            maxPoints = Math.max(maxPoints, vertical);
+//            ans = Math.max(ans, maxPoints + overlap + 1);
+//        }
+//        return ans;
+//    }
+//
+//    int gcd(int a, int b) {
+//        if (b == 0) {
+//            return a;
+//        } else {
+//            return gcd(b, a % b);
+//        }
+//    }
+//}
+class Line {
+    public double a, b, c;
 
-            for (int j = i + 1; j < points.length; j++) {
-                if (points[i].x == points[j].x) {
-                    if (points[i].y == points[j].y) {
-                        overlap++;
-                    } else {
-                        vertical++;
-                    }
-                    continue;
-                }
-                int dx = points[i].x - points[j].x;
-                int dy = points[i].y - points[j].y;
-                int tmp = gcd(dx, dy);
-                dx /= tmp;
-                dy /= tmp;
-                String k = dy + "/" + dx;
-
-                if (!slope.containsKey(k)) {
-                    slope.put(k, 0);
-                }
-                slope.put(k, slope.get(k) + 1);
-                maxPoints = Math.max(maxPoints, slope.get(k));
-            }
-            maxPoints = Math.max(maxPoints, vertical);
-            ans = Math.max(ans, maxPoints + overlap + 1);
-        }
-        return ans;
+    public Line(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
-    int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
+    public Line(int x1, int y1, int x2, int y2) {
+        if (x1 == x2) {
+            if (x1 == 0) {
+                a = 1;
+                b = 0;
+                c = 0;
+            } else {
+                a = 1.0 / x1;
+                b = 0;
+                c = 1;
+            }
+        } else if (y1 == y2) {
+            if (y1 == 0) {
+                a = 0;
+                b = 1;
+                c = 0;
+            } else {
+                a = 0;
+                b = 1.0 / y1;
+                c = 1;
+            }
         } else {
-            return gcd(b, a % b);
+            if (x1 * y2 == x2 * y1) {
+                a = 1;
+                b = -1.0 * (y1 - y2) / (x1 - x2);
+                c = 0;
+            } else {
+                a = 1.0 * (y1 - y2) / (x2 * y1 - x1 * y2);
+                b = 1.0 * (x1 - x2) / (x1 * y2 - x2 * y1);
+                c = 1;
+            }
         }
+    }
+
+    public String toString() {
+        return Double.toString(a) + " " + Double.toString(b) + " " + Double.toString(c);
+    }
+}
+
+public class Solution186 {
+
+    public int maxPoints(Point[] points) {
+        if (points.length < 2) {
+            return points.length;
+        }
+
+        HashMap<String, Integer> hash = new HashMap<String, Integer>();
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                Line line = new Line(points[i].x, points[i].y,
+                        points[j].x, points[j].y);
+                String key = line.toString();
+                if (hash.containsKey(key)) {
+                    hash.put(key, hash.get(key) + 1);
+                } else {
+                    hash.put(key, 1);
+                }
+            }
+        }
+
+        int max = 0;
+        String maxKey = "";
+        for (String key : hash.keySet()) {
+            if (hash.get(key) > max) {
+                max = hash.get(key);
+                maxKey = key;
+
+            }
+        }
+        String[] params = maxKey.split(" ");
+        double a = Double.parseDouble(params[0]);
+        double b = Double.parseDouble(params[1]);
+        double c = Double.parseDouble(params[2]);
+
+        int count = 0;
+        for (int i = 0; i < points.length; i++) {
+            if (Math.abs(a * points[i].x + b * points[i].y - c) < 1e-6) {
+                count++;
+            }
+        }
+        return count;
     }
 }
