46a9e6faaf4132b5d8c7152c1f2c310da9ee0ceb
YangAidi <598671587@qq.com>
2018-04-03 19:32:17 +0800

    problem450

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,20 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        Solution831 solution831=new Solution831();
-        System.out.println(solution831.threeSum2(1));
+        ListNode p1=new ListNode(1);
+        ListNode p2=new ListNode(2);
+        ListNode p3=new ListNode(3);
+        ListNode p4=new ListNode(4);
+        ListNode p5=new ListNode(5);
+        p1.next=p2;
+        p2.next=p3;
+        p3.next=p4;
+        p4.next=p5;
+        p5.next=null;
+        Solution450 solution450=new Solution450();
+        System.out.println(solution450.reverseKGroup(p1,2));
+//        Solution831 solution831=new Solution831();
+//        System.out.println(solution831.threeSum2(1));
 //        Solution186 solution186=new Solution186();
 //        Point[] p=new Point[]{new Point(-54,-297),new Point(-36,-222),new Point(3,-2),
 //                new Point(30,53),new Point(-5,1),new Point(-36,-222),new Point(0,2),
---
+++ src/Solution450.java
@@ -0,0 +1,99 @@
+/**
+ * @program: LintCode
+ * @author: YangAidi
+ * @create: 2018-04-03 18:32
+ **/
+
+//public class Solution450 {
+//    /**
+//     * @param head: a ListNode
+//     * @param k:    An integer
+//     * @return: a ListNode
+//     */
+//    public ListNode reverseKGroup(ListNode head, int k) {
+//        // write your code here
+//        if (head == null || head.next == null || k == 0 || k == 1) return head;
+//        ListNode nextHead = head;
+//        ListNode p = null;
+//        ListNode newHead = head;
+//        while (nextHead != null) {
+//            head=nextHead;
+//            //nextHead移动到下一轮的开头
+//            int i = 1;
+//            //前进k步
+//            while (nextHead != null&&i % (k+1) != 0 ) {
+//                nextHead = nextHead.next;
+//                ++i;
+//            }
+//            ListNode l = new ListNode(-1);
+//            while (head != null) {
+//                p = head;
+//                head = head.next;
+//                p.next = l.next;
+//                l.next = p;
+//            }
+//            if (i == k) {
+//                newHead = l.next;
+//            }
+//        }
+//
+//        return newHead.next;
+//    }
+//}
+public class Solution450 {
+    /**
+     * @param head a ListNode
+     * @param k an integer
+     * @return a ListNode
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        head = dummy;
+        while (true) {
+            head = reverseK(head, k);
+            if (head == null) {
+                break;
+            }
+        }
+
+        return dummy.next;
+    }
+
+    // head -> n1 -> n2 ... nk -> nk+1
+    // =>
+    // head -> nk -> nk-1 .. n1 -> nk+1
+    // return n1
+    public ListNode reverseK(ListNode head, int k) {
+        ListNode nk = head;
+        for (int i = 0; i < k; i++) {
+            if (nk == null) {
+                return null;
+            }
+            nk = nk.next;
+        }
+
+        if (nk == null) {
+            return null;
+        }
+
+        // reverse
+        ListNode n1 = head.next;
+        ListNode nkplus = nk.next;
+
+        ListNode prev = null;
+        ListNode curt = n1;
+        while (curt != nkplus) {
+            ListNode temp = curt.next;
+            curt.next = prev;
+            prev = curt;
+            curt = temp;
+        }
+
+        // connect
+        head.next = nk;
+        n1.next = nkplus;
+        return n1;
+    }
+}
